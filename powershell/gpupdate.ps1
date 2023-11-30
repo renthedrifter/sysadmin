20df2e362440884e682d0fad51fb3efb3c06b63d
renthedrifter <araan1@gmail.com>
2023-11-29 21:15:40 -0500

    Updated a gpupdate script

--- powershell/gpupdate.ps1
+++ powershell/gpupdate.ps1
@@ -1,7 +1,8 @@
+# List all computers.
 $all = "list", "of", "computers"
 
 write-host "something" -foregroundcolor Yellow
-$computername = read-host -prompt "Updateing GPO on $computername"
+$computername = read-host -prompt "Updating GPO on $computername"
 if ($computername -eq 'all'){
 invoke-command -computername $all -scriptblock {hostname; gpupdate /force}
 }
