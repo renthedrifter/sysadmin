1b71a4866d3ece66eeeb47b73f8f641c1500f078
renthedrifter <araan1@gmail.com>
2023-12-08 13:08:15 -0500

    Added a resource file, DB directory.

--- powershell/copy-item.ps1
+++ powershell/copy-item.ps1
@@ -1,8 +1,9 @@
 # HAS NOT BEEN TESTED YET!!!!
+# Start putting items in c:\temp then possibly have a new script to check for items in c:\temp.
 
 $item = "path to item"
 $computer =  "computer name"
 $session = New-PSSession $computer
 
-Copy-Item $item -Destination c:\ -ToSession $session
+Copy-Item $item -Destination c:\temp -ToSession $session
 $session | Remove-PSSession
