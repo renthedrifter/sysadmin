95c4cbb24182252aecf2a6edaf8077525f91167d
aaron <aaronfitts@gmail.com>
2024-03-05 11:59:30 -0500

    rust

--- bash/mftanalyze.sh
+++ bash/mftanalyze.sh
@@ -11,12 +11,12 @@ read image
 # Runs mmls first for $mft offset number.
 mmls $image
 
+# Run icat to create the mft image.
+icat -o $offset $image 0 > mft.raw
+
 echo what is the mft offset not including preceding 0s.
 
 read offset
-
-# Run icat to create the mft image.
-icat -o $offset $image 0 > mft.raw
 
 # Parse with analyzeMFT.
 analyzeMFT.py -f mft.raw -o mftanalyzed.csv
